0ccbf16b09b62968a52d3f3e5f16ea0c10133aa7
Vincent Hardy <vhardy@adobe.com>
2011-10-26 18:48:15 -0700

    Updated style

--- custom/style-toggle.js
+++ custom/style-toggle.js
@@ -7,25 +7,33 @@
     function toggleStyle() {
         var st = document.getElementById('st');
 
-        if (st.disabled === true) {
-            st.disabled = false;
+        if (st.hasAttribute('disabled') === true) {
+            st.removeAttribute('disabled');
             toggle.textContent = defaultStyleText;
         } else {
-            st.disabled = true;
+            st.setAttribute('disabled', 'true');
             toggle.textContent = additionalStyleText;
         }
     }
 
     window.onload = function() {
+        var st = document.getElementById('st');
         var divHead = document.getElementById('div-head');
+        var defaultText = additionalStyleText;
+        
+        if (st.hasAttribute('disabled') === false) {
+            defaultText = defaultStyleText;
+        }
+        
         toggle = document.createElement('a');
 
 		divHead.insertBefore(toggle, divHead.firstChild);
-        toggle.textContent = defaultStyleText;
+        toggle.textContent = defaultText;
         toggle.setAttribute('class', 'toggle');
         toggle.setAttribute('href', '#');
         toggle.addEventListener("click", toggleStyle, false);
     };
     
 })();
+
 
